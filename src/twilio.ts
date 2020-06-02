4f0bba5b0ddaa86f35002a0e079f59306818d7b3
Daniel Simpson <daniel_simpson1988@hotmail.com>
2020-06-02 17:44:06 +1000

    Add voicemail handler and text notification

--- src/twilio.ts
+++ src/twilio.ts
@@ -4,6 +4,8 @@ import {
 } from "@twilio-labs/serverless-runtime-types/types";
 
 export type EnvironmentVariables = {
+  CLIENT_PHONE: string;
+  TWILIO_PHONE: string;
   VOICEMAIL_MESSAGE: string;
 };
 
@@ -18,3 +20,42 @@ export type CallHandler = ServerlessFunctionSignature<
   CallEvent
 >;
 
+export type VoicemailEvent = {
+  From: string;
+  TranscriptionStatus: "completed" | "failed";
+  TranscriptionText: string;
+  RecordingUrl: string;
+};
+
+export type VoicemailHandler = ServerlessFunctionSignature<
+  EnvironmentVariables,
+  VoicemailEvent
+>;
+
+export function GetFromPhoneNumber(
+  context: EnvironmentVariables,
+  event: { From: string }
+) {
+  if (event.From && event.From.length > 0) {
+    return event.From;
+  }
+
+  return context.TWILIO_PHONE;
+}
+
+export function SendTextMessage(
+  context: Context<EnvironmentVariables>,
+  event: { From: string },
+  message: string
+) {
+  const from = GetFromPhoneNumber(context, event);
+
+  context
+    .getTwilioClient()
+    .messages.create({
+      from,
+      to: context.CLIENT_PHONE,
+      body: message,
+    })
+    .catch((ex) => console.error(ex));
+}
