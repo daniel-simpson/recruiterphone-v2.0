a7dfc387f2967bb2e1aef7a7c01bbfb06d71cceb
Daniel Simpson <daniel_simpson1988@hotmail.com>
2020-06-15 21:25:39 +1000

    add guard against sending text messages without correctly formed request

--- src/voicemailNotify.ts
+++ src/voicemailNotify.ts
@@ -2,8 +2,18 @@ import "@twilio-labs/serverless-runtime-types";
 import { VoicemailHandler, SendTextMessage } from "./twilio";
 
 export const handler: VoicemailHandler = function (context, event, callback) {
-  let message;
+  if (
+    !event ||
+    !event.From ||
+    event.From.length == 0 ||
+    !event.RecordingUrl ||
+    event.RecordingUrl.length == 0
+  ) {
+    callback("Missing arguments");
+    return;
+  }
 
+  let message;
   if (event.TranscriptionStatus == "completed") {
     message = `Voicemail from ${event.From}: "${event.TranscriptionText}". ${event.RecordingUrl}`;
   } else {
